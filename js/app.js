9ffefe5d1ef1ee762485620e13710b93692ff4cf
webnur <mdnuruli291@gmail.com>
2022-09-03 05:34:57 +0600

    api call in app js file

---
+++ js/app.js
@@ -0,0 +1,22 @@
+
+const loadNews = async () => {
+    const url =`https://openapi.programming-hero.com/api/news/categories`;
+    const response = await fetch(url);
+    const data = await response.json()
+
+    // console.log(data.data.news_category)
+    return data.data.news_category;
+
+
+}
+
+const displaycategoryName = async () => {
+    const categories = await loadNews() 
+    // console.log(categories)
+
+    categories.forEach(category => {
+        console.log(category)
+    })
+}
+
+displaycategoryName()
