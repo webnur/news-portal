f37b7d35f0f86e2fabd432e79e40764bf17d05e8
webnur <mdnuruli291@gmail.com>
2022-09-03 09:33:40 +0600

    single post design

--- js/app.js
+++ js/app.js
@@ -46,33 +46,27 @@ const displayCategoryData = posts => {
         postDiv.classList.add('card', 'lg:card-side', 'bg-base-100', 'shadow-xl');
 
         postDiv.innerHTML = `
-            <figure><img src="${thumbnail_url}" alt="Album"></figure>
-            <div class="card-body">
-            <h2 class="card-title">${title} ${category_id}</h2>
-            <p>${details}</p>
-            <div>
-                <!-- author picture  -->
-                <div class="md:flex items-center">
-                <img src="${author.img}" class="rounded-full w-24" />
-                <h5 class="ml-2">a${author.name}</h5>
-                <div class="ml-8">
-                    
-                    <h4 class="text-3xl"><i class="fa-regular fa-eye"></i>${total_view}</h4>
-                </div>
-                <div class="rating rating-lg ml-8">
-                    <input type="radio" name="rating-9" class="rating-hidden" />
-                    <input type="radio" name="rating-9" class="mask mask-star-2" />
-                    <input type="radio" name="rating-9" class="mask mask-star-2" checked />
-                    <input type="radio" name="rating-9" class="mask mask-star-2" />
-                    <input type="radio" name="rating-9" class="mask mask-star-2" />
-                    <input type="radio" name="rating-9" class="mask mask-star-2" />
-                </div>
-                </div>
-            </div>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">Listen</button>
-            </div>
-            </div>
+        <figure><img src="${thumbnail_url}" alt="Album"></figure>
+        <div class="card-body">
+          <h2 class="card-title">${title}</h2>
+          <p>${details.slice(0, 200)}</p>
+          <!-- author information div  -->
+          <div class="lg:flex items-center">
+            <img src="${author.img}" class="rounded-full w-20" />
+            <h4 class="ml-3">${author.name}</h4>
+            <h5 class="ml-5">view </h5>
+            <div class="rating ml-5">
+                <input type="radio" name="rating-1" class="mask mask-star" />
+                <input type="radio" name="rating-1" class="mask mask-star" checked />
+                <input type="radio" name="rating-1" class="mask mask-star" />
+                <input type="radio" name="rating-1" class="mask mask-star" />
+                <input type="radio" name="rating-1" class="mask mask-star" />
+              </div>
+          </div>
+          <div class="card-actions">
+            <button class="btn btn-primary">view Post</button>
+          </div>
+        </div>
         
         
         
